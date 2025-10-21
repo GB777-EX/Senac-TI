f0a65a23111e6d5012d93a2d5cea56e96f598f67
GB777-EX <gabrielsantos24082009@gmail.com>
2025-10-20 22:20:12 -0300

    att

--- Laços de repetição/pares/pares.js
+++ Laços de repetição/pares/pares.js
@@ -1,32 +1,18 @@
-let num = prompt("Digite um número:");
+let num = Number(prompt("Digite um número:"));
 let pares = 0;
-let texto 
-let fim
+let lista = "";
 
-
-if (num==2) {
-    texto = "Não existem números pares entre 0 e 2";
-} else {   
+for (let i = 1; i <= num; i++) {
+  if (i % 2 === 0) {
+    pares++;
+    lista += i + ", ";
+  }
 }
-if (pares % 2 == 1) {
-    pares = pares/2 
-    }else{
-        pares = (pares/2) - 1;
-} 
 
-pares = Math.floor(pares,0);
-if (num ==1) {
-    texto = `entre 0 e ${num} existe ${pares} numero par que é: ${cont}`;
-}else{
-    texto = `entre 0 e ${num} existem ${pares} numeros pares e são: ${cont}`;
-} 
-console.log(texto);
- 
-for (i=2; i < num; i+=2) {
-    if (i== num - 1 || i == num - 2) {
-        fim = ".";
-    } else {
-        fim = ", ";
-    }
-    console.log(i + fim);
-}    
+if (pares === 0) {
+  console.log(`Não existem números pares entre 1 e ${num}.`);
+} else if (pares === 1) {
+  console.log(`Entre 1 e ${num} existe 1 número par: ${lista.slice(0, -2)}.`);
+} else {
+  console.log(`Entre 1 e ${num} existem ${pares} números pares: ${lista.slice(0, -2)}.`);
+}
---
+++ exercicios fixação lacos (Perplexity)/do_while.js
@@ -0,0 +1,6 @@
+let num
+do {
+    num=prompt("Enter a number:");
+} while (num !== "0");
+
+console.log(`Numero ${num} Salvo com sucesso!`);
