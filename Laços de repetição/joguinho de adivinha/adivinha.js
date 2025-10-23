57416c171b1702597882fc6923c5515c27de0892
GB777-EX <gabrielsantos24082009@gmail.com>
2025-10-22 21:48:45 -0300

    Update adivinha.js

--- Laços de repetição/joguinho de adivinha/adivinha.js
+++ Laços de repetição/joguinho de adivinha/adivinha.js
@@ -27,20 +27,40 @@ switch (nivel) {
             }
         }
         if (acertou === false) {
-            alert("Suas tentativas acabaram! O número era " + nivelFacil);
+            alert(`Suas tentativas acabaram! O número era ${nivelFacil}`);
         }
         break;
-        case "2":
-            while(tentativaMedia > 0 && acertou ===false){
-                chuteMedio = parseInt(prompt(`Você tem ${tentativaMedia} tentativas. Qual o seu chute?`));
-                if(chuteMedio === nivelMedio){
-                    alert("Parabéns! Você acertou!");
-                    acertou = true;
-                }
-                else {
-                    alert("Errou! Tente novamente.");
-                    tentativaMedia--;
-                }
-                    
+    case "2":
+        while (tentativaMedia > 0 && acertou === false) {
+            chuteMedio = parseInt(prompt(`Você tem ${tentativaMedia} tentativas. Qual o seu chute?`));
+            if (chuteMedio === nivelMedio) {
+                alert("Parabéns! Você acertou!");
+                acertou = true;
+            } else {
+                alert("Errou! Tente novamente.");
+                tentativaMedia--;
+            }       
+        }
+        if (acertou === false) {
+            alert(`Suas tentativas acabaram! O número era ${nivelMedio}`);
+        }
+        break;
+    case "3":
+        while (tentativaDificil > 0 && acertou === false) {
+            chuteDificil = parseInt(prompt(`Você tem ${tentativaDificil} tentativas. Qual o seu chute?`));
+            if (chuteDificil === nivelDificil) {
+                alert("Parabéns! Você acertou!");
+                acertou = true;
+            } else {
+                alert("Errou! Tente novamente.");
+                tentativaDificil--;
             }
+        }
+        if (acertou === false) {
+            alert(`Suas tentativas acabaram! O número era ${nivelDificil}`);
+        }
+        break;
+    default:
+        alert("Nível inválido! Por favor, escolha 1, 2 ou 3.");
 }
+
